ffc3f86118758500bc8b4c15267ffde6bf40ce23
dogmom3 <blair.seivers@yahoo.com>
2022-01-22 10:47:17 -0500

    add js code

---
+++ script.js
@@ -0,0 +1,14 @@
+var dogName = window.prompt("What is your dog's name?");
+//what is this?
+console.log(dogName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our dog's name is " + dogName);
+
+//this creates a function named "run"
+function run() {
+    window.alert("The race has begun!");
+}
+//run();
