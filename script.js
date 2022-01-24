55eef4dc85a6212763aad65200e50c63e826485e
dogmom3 <blair.seivers@yahoo.com>
2022-01-24 15:33:01 -0500

    update jump function

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 //    * Out jump each opponent dog
 // "LOSE" - Player dog's bones are zero or less
 
-var playerName = window.prompt("What is your dog's name?");
+
 var playerBones = 100;
 var playerJump = 10;
 var playerMoney = 10;
@@ -16,86 +16,84 @@ var opponentNames = ["Waldo", "Arlene", "Violet"];
 var opponentBones = 50;
 var opponentJump = 12;
 
-//jump function
-var jump = function(opponentName) {
-    //jump function statements
-    // repeat and execute as long as the opponent-dog still has bones
-    while(opponentBones > 0) { 
-        //ask player if they'd like to jump or run
-        var promptJump = window.prompt("Would you like to JUMP or SKIP this round? Enter 'JUMP' or 'SKIP' to choose.");
-    }
-        // if player chooses to jump, then jump
-        if (promptJump === "jump" || promptJump === "JUMP") {
-        //remove opponents bones by subtracting the amount set in the playerJump variable
-        opponentBones = opponentBones - playerJump;
-            console.log(
-            playerName + " jumped higher than " + opponentName + ". " + opponentName + " now has " + opponentBones + " bones remaining."
-);
-        }
-    
-        for(var i = 0; i < opponentNames.length; i++) {
-        console.log(opponentNames[i]);
-        console.log(i);
-        console.log(opponentNames[i] + " is at " + i + " index");
-}
-
 //alert players that they are starting the round
 window.alert("Welcome to Dog Olympics!");
+var playerName = window.prompt("What is your dog's name?");
 
-//ask player if they'd like to jump or run
-var promptJump = window.prompt("Would you like to JUMP or SKIP this round? Enter 'JUMP' or 'SKIP' to choose.");
+//jump function
+var jump = function (opponentName) {
+    //jump function statements
+    // repeat and execute as long as the opponent-dog still has bones
+    while (playerBones > 0 && opponentBones > 0) {
+        //ask player if they'd like to jump or run
+        var promptJump = window.prompt("Would you like to JUMP or SKIP this round? Enter 'JUMP' or 'SKIP' to choose.");
+        
+        //if player picks "skip" confirm and then stop the loop
+        if (promptJump === "skip" || promptJump === "SKIP") {
+            //confirm player wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-// if player chooses to jump, then jump
-if (promptJump === "jump" || promptJump === "JUMP") {
-    //remove opponents bones by subtracting the amount set in the playerJump variable
-    opponentBones = opponentBones - playerJump;
-        console.log(
-        playerName + " jumped higher than " + opponentName + ". " + opponentName + " now has " + opponentBones + " bones remaining."
-);
+            //if yes (true), leave game
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this round. Arf! Arf!");
+                //subtract money from playerMoney for skipping
+                playerMoney = playerMoney - 10;
+                console.log("playerMoney", playerMoney);
+                break;
+            }
+        }
 
-//count opponents bones
-if (opponentBones <=0) {
-    window.alert(opponentName + " has lost!");
-        } else {
-            window.alert(opponentName + " still has " + opponentBones + " bones left.");
-};
+            //remove opponents bones by subtracting the amount set in the playerJump variable
+            opponentBones = opponentBones - playerJump;
+            console.log(
+                playerName + " jumped higher than " + opponentName + ". " + opponentName + " now has " + opponentBones + " bones remaining."
+            );
 
-//remove players bones by subtracting the amount set in opponentJump variable
-    playerBones = playerBones - opponentJump;
-    console.log(
-        opponentName + " jumped " + playerName + ". " + playerName + " now has " + playerBones + " bones remaining."
-);
+            //count opponents bones
+            if (opponentBones <= 0) {
+                window.alert(opponentName + " has lost!");
 
-//count players bones
-if (playerBones <= 0) {
-        window.alert(playerName + " has lost!");
+                //award player money for wining
+                playerMoney = playerMoney + 20;
+
+                //leave while() loop since opponent has lost
+                break;
+            } else {
+                window.alert(opponentName + " still has " + opponentBones + " bones left.");
+            }
+
+            //remove players bones by subtracting the amount set in opponentJump variable
+            playerBones = playerBones - opponentJump;
+            console.log(
+                opponentName + " jumped " + playerName + ". " + playerName + " now has " + playerBones + " bones remaining."
+            );
+
+            //count players bones
+            if (playerBones <= 0) {
+                window.alert(playerName + " has lost!");
+                //leave while() loop if player has lost
+                break;
             } else {
                 window.alert(playerName + " still has " + playerBones + " bones left.");
-}
-    
-//if player chooses to skip
-} else if (promptJump === "skip" || promptJump === "SKIP"){
-    //confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+            }
+        }
+    };
 
-//if yes (true), leave game
-if (confirmSkip) {
-     window.alert(playerName + " has decided to skip this round. Arf! Arf!");
-        //subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 2;
-}
-//if no (false), ask question again by running jump () again
-else {
-    jump();
-}
+//             //if no (false), ask question again by running jump () again
+//          else jump();
+//     }
 
-//if player did not choose 1 or 2 in prompt
-} else {
-    window.alert("You need to pick a valid option. Try again.");
-}
-};
+//     //if player did not choose 1 or 2 in prompt
+// } else {
+//     window.alert("You need to pick a valid option. Try again.");
+//         }
+//     }
+// };
 
 //for loop
-for(var i = 0; i < opponentNames.length; i++) {
-    jump(opponentNames[i]);
+for (var i = 0; i < opponentNames.length; i++) {
+    var pickedOpponentName = opponentNames[i];
+    opponentBones = 50;
+    //call function with opponent dog
+    jump(pickedOpponentName[i]);
 }
